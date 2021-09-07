3f0c56f986c5829e422dd49562a752297e3da934
Hamza5743 <53045133+Hamza5743@users.noreply.github.com>
2021-09-07 12:31:41 +0500

    Add files via upload

--- Scripts/Calculator.js
+++ Scripts/Calculator.js
@@ -1,83 +1,134 @@
-function filter(CourseNames, Sections, AllowedPeriods){
-    let iterations = Courses.length;
-    var preci = -1;
-    var presi = -1;
-    CourseSectionsSort = [];
-    CourseTypeSort = [];
-    Filtered = [];
-    BannedCourse = "0000000";
-    BannedSection = "0000000";
-    for (let i = 0; i < iterations; i++) {
-        var csvs = Courses[i].split("|");
-        var tvalid = true;
-        var stime = Math.floor(csvs[2]/90);
-        var etime = Math.ceil(csvs[3]/90);
-        if ((BannedCourse.includes(csvs[0]) || csvs[0].includes(BannedCourse)) && (BannedSection.includes(csvs[1]) || csvs[1].includes(BannedSection))){
-            tvalid = false;
+function ProduceTimeTable(){
+    var HtmlCourses = document.getElementsByClassName("CourseName");
+    var CourseSections = ExtractSections();
+    var allowedPeriods = ExtractAllowedTimes();
+    var CourseNames = [];
+    var NumCourses = 0;
+
+    for (let i = 0; i < HtmlCourses.length; i++) {
+        CourseNames.push(HtmlCourses[i].value);
+        if (CourseSections[i].length != 0 && HtmlCourses[i].value != "Please Select a Course."){
+            NumCourses++;
         }
-        for (let j = stime; j < etime; j++) {
-            if (AllowedPeriods[csvs[4]][j] == false){
-                tvalid = false;
-                BannedCourse = csvs[0];
-                BannedSection = csvs[1];
-                if (preci != -1){
-                    if ((CourseNames[preci].includes(csvs[0]) || csvs[0].includes(CourseNames[preci])) && (Sections[preci].split("|")[presi].includes(csvs[1]) || csvs[1].includes(Sections[preci].split("|")[presi]))){
-                        CourseSectionsSort = ["Banned"];
+    }
+
+    var filtered = filter(CourseNames, CourseSections, allowedPeriods);
+
+    var Pdiv = document.getElementById("Output");
+    var Ediv = document.getElementById("Error");
+    Pdiv.innerHTML = "";
+    Ediv.innerHTML = "";
+    if (filtered.length < NumCourses || NumCourses == 0){
+        var Error = document.createElement("h3");
+        Error.innerHTML = "No Suitable TimeTable Found!";
+        Ediv.appendChild(Error);
+    }
+    else{
+        var TimeTable = [];
+        for (let i = 0; i < 6; i++) {
+            TimeTable.push(["-","-","-","-","-","-","-","-"]);
+        }
+
+        GenerateTable(filtered, 0, TimeTable);
+        var Error = document.createElement("h3");
+
+        if (Pdiv.childElementCount == 0){
+            Error.innerHTML = "No Suitable TimeTable Found!";
+        }
+        else{
+            Error.innerHTML = "Generated Scoll Down!";
+        }
+        Ediv.appendChild(Error);
+    }
+}
+
+function ExtractAllowedTimes()
+{
+    var AllowedPeriods = [];
+    for (let i = 0; i < 6; i++) {
+        let row = [];
+        for (let j = 0; j < 8; j++) {
+            row.push(document.getElementById(String(i) + String(j)).checked);
+        }
+        AllowedPeriods.push(row);
+    }
+    return AllowedPeriods;
+}
+
+function SameCourses(c1, c2)
+{
+    return c1.includes(c2) || c2.includes(c1);
+}
+
+function SameSection(sections, s){
+    for (let i = 0; i < sections.length; i++) {
+        if (sections[i].substring(0, 6) == s.substring(0, 6)){
+            return true;
+        }
+    }
+    return false;
+}
+
+function filter(courseNames, sections, allowedPeriods){
+    var currentCourse = [];
+    var filtered = [];
+    for (let i = 0; i < Courses.length;) {
+        var iterate = true;
+        for (let j = 0; j < 5; j++) {
+            var previousSection;
+            var currentSection = [];
+            if (i < Courses.length && Courses[i][0] == courseNames[j] && SameSection(sections[j], Courses[i][1]))
+            {
+                previousSection = Courses[i][1].substring(0, 6);
+                while(i < Courses.length && SameCourses(Courses[i][0], courseNames[j])){
+                    var stime = Math.floor(parseInt(Courses[i][2])/90);
+                    var etime = Math.ceil(parseInt(Courses[i][3])/90);
+                    var allowed = true;
+                    if (previousSection != Courses[i][1].substring(0, 6) && currentSection.length != 0)
+                    {
+                        previousSection = Courses[i][1].substring(0, 6);
+                        currentCourse.push(currentSection);
+                        currentSection = [];
                     }
-                }
-            }
-        }
-        if (tvalid == true){
-            for (let j = 0; j < CourseNames.length; j++) {
-                if (csvs[0].includes(CourseNames[j])) {
-                    IndividualSections = Sections[j].split("|");
-                    for (let k = 0; k < IndividualSections.length; k++) {
-                        if (csvs[1].includes(IndividualSections[k])){
-                            if (CourseSectionsSort.length == 0 && CourseTypeSort.length == 0 && Filtered.length == 0){
-                                CourseSectionsSort.push(Courses[i]);
+                    if (SameSection(sections[j], Courses[i][1]) == false)
+                    {
+                        allowed = false;
+                    }
+                    for (let k = stime; k < etime; k++) {
+                        if (allowedPeriods[parseInt(Courses[i][4])][k] == false || allowed == false)
+                        {
+                            var wrongSection = Courses[i][1].substring(0, 6);
+                            allowed = false;
+                            currentSection = [];
+                            while(i < Courses.length && SameCourses(Courses[i][0], courseNames[j]) && Courses[i][1].substring(0, 6) == wrongSection){
+                                i++;
                             }
-                            else if (CourseSectionsSort[0] == "Banned"){
-                                if ((preci == j && preci != k) || preci != j){
-                                    if (preci != j && CourseTypeSort.length != 0){
-                                        Filtered.push(CourseTypeSort);
-                                        CourseTypeSort = [];
-                                    }
-                                    CourseSectionsSort = [];
-                                    CourseSectionsSort.push(Courses[i]);
-                                }
-                            }
-                            else{
-                                if (preci == j){
-                                    if (presi == k){
-                                        CourseSectionsSort.push(Courses[i]);
-                                    }
-                                    else{
-                                        CourseTypeSort.push(CourseSectionsSort);
-                                        CourseSectionsSort = [];
-                                        CourseSectionsSort.push(Courses[i]);
-                                    }
-                                }
-                                else{
-                                    CourseTypeSort.push(CourseSectionsSort);
-                                    Filtered.push(CourseTypeSort);
-                                    CourseTypeSort = [];
-                                    CourseSectionsSort = [];
-                                    CourseSectionsSort.push(Courses[i]);
-                                }
-                            }
-                            preci = j;
-                            presi = k;
+                            previousSection = Courses[i][1].substring(0, 6);
+                            i--;
+                            break;
                         }
                     }
+                    if (allowed)
+                    {
+                        currentSection.push(Courses[i]);
+                    }
+                    iterate = false;
+                    i++;
+                }
+                if (currentSection.length != 0){
+                    currentCourse.push(currentSection);
+                }
+                if (currentCourse.length != 0){
+                    filtered.push(currentCourse);
+                    currentCourse = [];
                 }
             }
         }
+        if (iterate){
+            i++;
+        }
     }
-    if (CourseSectionsSort.length != 0 && CourseSectionsSort[0] != "Banned"){
-        CourseTypeSort.push(CourseSectionsSort);
-        Filtered.push(CourseTypeSort);
-    }
-    return Filtered;
+    return filtered;
 }
 
 function PrintTable(TimeTable){
@@ -118,12 +169,11 @@ function GenerateTable(Filtered, Depth, TimeTable){
     for (let i = 0; i < Filtered[Depth].length; i++) {
         let clash = false;
         for (let j = 0; j < Filtered[Depth][i].length; j++) {
-            var csvs = Filtered[Depth][i][j].split("|");
-            var stime = Math.floor(csvs[2]/90);
-            var etime = Math.ceil(csvs[3]/90);
+            var stime = Math.floor(parseInt(Filtered[Depth][i][j][2])/90);
+            var etime = Math.ceil(parseInt(Filtered[Depth][i][j][3])/90);
             for (let k = stime; k < etime; k++) {
-                if (TimeTable[csvs[4]][k] == "-"){
-                    TimeTable[csvs[4]][k] = csvs[0] + " " + csvs[1] + " " + csvs[5];
+                if (TimeTable[parseInt(Filtered[Depth][i][j][4])][k] == "-"){
+                    TimeTable[parseInt(Filtered[Depth][i][j][4])][k] = Filtered[Depth][i][j][0] + "\n" + Filtered[Depth][i][j][1] + "\n" + Filtered[Depth][i][j][5];
                 }
                 else{
                     clash = true;
@@ -134,120 +184,107 @@ function GenerateTable(Filtered, Depth, TimeTable){
             GenerateTable(Filtered, Depth + 1, TimeTable);
         }
         for (let j = 0; j < Filtered[Depth][i].length; j++) {
-            var csvs = Filtered[Depth][i][j].split("|");
-            var stime = Math.floor(csvs[2]/90);
-            var etime = Math.ceil(csvs[3]/90);
+            var stime = Math.floor(parseInt(Filtered[Depth][i][j][2])/90);
+            var etime = Math.ceil(parseInt(Filtered[Depth][i][j][3])/90);
             for (let k = stime; k < etime; k++) {
-                if (TimeTable[csvs[4]][k] == csvs[0] + " " + csvs[1] + " " + csvs[5]){
-                    TimeTable[csvs[4]][k] = "-";
+                if (TimeTable[parseInt(Filtered[Depth][i][j][4])][k] == Filtered[Depth][i][j][0] + "\n" + Filtered[Depth][i][j][1] + "\n" + Filtered[Depth][i][j][5]){
+                    TimeTable[parseInt(Filtered[Depth][i][j][4])][k] = "-";
                 }
             }
         }
     }
 }
 
-function ProduceTimeTable(){
-    var Pdiv = document.getElementById("Output");
-    var Ediv = document.getElementById("Error");
-    Pdiv.innerHTML = "";
-    Ediv.innerHTML = "";
-    var AllowedPeriods = [];
-    for (let i = 0; i < 6; i++) {
-        let row = [];
-        for (let j = 0; j < 8; j++) {
-            row.push(document.getElementById(String(i) + String(j)).checked);
-        }
-        AllowedPeriods.push(row);
-    }
-    var HtmlCourses = document.getElementsByClassName("CourseName");
-    var HtmlSections = document.getElementsByClassName("CourseSection");
-    CourseNames = [];
-    CourseSections = [];
-    var NumCourses = 0;
-    for (let i = 0; i < HtmlCourses.length; i++) {
-        CourseNames.push(HtmlCourses[i].value);
-        CourseSections.push(HtmlSections[i].value);
-        if (HtmlSections[i].value != "" && HtmlCourses[i].value != "Please Select a Course."){
-            NumCourses++;
-        }
-    }
-    Filtered = filter(CourseNames, CourseSections, AllowedPeriods);
-    if (Filtered.length < NumCourses){
-        var Pdiv = document.getElementById("Error");
-        var Error = document.createElement("h3");
-        Error.innerHTML = "No Suitable TimeTable Found!";
-        Pdiv.appendChild(Error);
-    }
-    else{
-        TimeTable = [];
-        for (let i = 0; i < 6; i++) {
-            TimeTable.push(["-","-","-","-","-","-","-","-"]);
-        }
-        GenerateTable(Filtered, 0, TimeTable);
-        var Ediv = document.getElementById("Error");
-        var Pdiv = document.getElementById("Output");
-        var Error = document.createElement("h3");
-        if (Pdiv.childElementCount == 0){
-            Error.innerHTML = "No Suitable TimeTable Found!";
-        }
-        else{
-            Error.innerHTML = "Generated Scoll Down!";
-        }
-        Ediv.appendChild(Error);
-    }
-}
-
-function AddCourseSelection(){
-    var select = document.getElementsByClassName("CourseName");
-    for (let i = 0; i < select.length; i++) {
-        prev = "";
-        Courses.forEach(element => {
-            var csvs = element.split("|");
-            if (csvs[0] != prev && csvs[0].includes("Lab") != true){
-                var option = document.createElement("option");
-                option.value = csvs[0];
-                option.text = csvs[0];
-                select[i].appendChild(option);
-                prev = csvs[0];
+function ExtractSections()
+{
+    var checkedSections = [];
+    for (let i = 1; i < 6; i++) {
+        var sections = document.getElementsByClassName("c" + i + "s");
+        var secs = [];
+        for (let check of sections){
+            if (check.checked)
+            {
+                secs.push(check.name);
             }
-        });
+        }
+        checkedSections.push(secs);
     }
+    return checkedSections;
 }
 
 function AddSections(Course){
-    var cclass = Course.className;
-    cclass = cclass.split(" ")[1];
-    var select = document.getElementById(cclass);
-    var tbox = document.getElementById("s" + cclass[1]);
-    tbox.value = "";
-    for (let i = select.length - 1; i > 0; i--) {
-        select.remove(i);
+    var table = document.getElementById(Course.className.split(" ")[1]);
+    var first = table.firstElementChild;
+    while (first)
+    {
+        first.remove();
+        first = table.firstElementChild;
     }
     prev = "";
-    Courses.forEach(element => {
-        var csvs = element.split("|");
-        if (csvs[0] == Course.value && csvs[1] != prev){
-            var option = document.createElement("option");
-            option.value = csvs[1];
-            option.text = csvs[1];
-            select.appendChild(option);
-            prev = csvs[1];
+    var counter = 5;
+    var currentRow;
+    Courses.forEach(period => {
+        var section = period[1];
+        if (period[0] == Course.value && section != prev){
+            if (counter >= 4)
+            {
+                var row = document.createElement("div");
+                row.className = "list-group list-group-horizontal-sm flex-fill";
+                row.style = "margin-bottom: 0px;"
+                table.appendChild(row);
+                currentRow = row;
+                counter = 0;
+            }
+            var label = document.createElement("label");
+            label.className = "list-group-item";
+            label.style = "margin-bottom: 0px; min-width: 25%";
+            var check = document.createElement("input");
+            check.type = "checkbox";
+            check.className = "form-check-input me-1 " + table.id + "s";
+            check.value = "";
+            check.style = "margin-left: auto; position: static;";
+            check.defaultChecked = true;
+            check.name = section;
+            label.appendChild(check);
+            label.innerHTML += "\n&nbsp&nbsp&nbsp" + section;
+            currentRow.appendChild(label);
+            prev = section;
+            counter += 1;
         }
     });
 }
 
-function AddToText(Select){
-    var sid = Select.getAttribute('id');
-    var tbox = document.getElementById("s" + sid[1]);
-    for (let i = Select.length - 1; i >= 0; i--) {
-        if (Select[i].selected == true){
-            if (tbox.value == ""){
-                tbox.value = Select[i].value;
+function ToggleRow(check)
+{
+    var row = check.id[1];
+    for (let i = 0; i < 8; i++) {
+        var box = document.getElementById(row + i);
+        box.checked = check.checked;
+    }
+}
+
+function ToggleCol(check)
+{
+    var col = check.id[1];
+    for (let i = 0; i < 6; i++) {
+        var box = document.getElementById(i + col);
+        box.checked = check.checked;
+    }
+}
+
+window.onload = function AddCourseSelection(){
+    var select = document.getElementsByClassName("CourseName");
+    for (let i = 0; i < select.length; i++) {
+        prev = "";
+        Courses.forEach(element => {
+            var Course = element[0];
+            if (Course != prev && Course.includes("Lab") != true){
+                var option = document.createElement("option");
+                option.value = Course;
+                option.text = Course;
+                select[i].appendChild(option);
+                prev = Course;
             }
-            else{
-                tbox.value = tbox.value + "|" + Select[i].value;
-            }
-            Select.remove(i);
-        }
+        });
     }
 }
